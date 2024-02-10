09d202fbf030c07d53f1653b7400e01297b72fc3
ZeroxyDev <ZeroxyDev@gmail.com>
2024-02-10 02:05:22 +0100

    [Add] Web and App style fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Titlebar from "./components/titlebar";
 import { MCDUContextProvider } from "./context/mcduContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,19 +16,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
+  const [isTauri, setIsTauri] = useState<boolean>(false);
+
   useEffect(() => {
     document.addEventListener('contextmenu', event => event.preventDefault());
+    if (window?.__TAURI__) {
+      console.log('Tauri detected');
+      setIsTauri(true);
+    }
   }, []);
-  
+
   return (
     <html lang="en">
-      <body className={"rounded-[40px] m-0 bg-transparent overflow-hidden"}>
-        <div className='bg-background w-full h-[600px] flex justify-center items-center bordernone border-tertiary rounded-big'>
-        <div className='h-12 block'><Titlebar /></div>
+      <body className={`rounded-[40px] m-0 ${isTauri ? 'bg-transparent' : 'bg-background'} overflow-hidden`}>
+        <div style={{ height: isTauri ? '600px' : '100vh' }} className={`bg-background w-full ${isTauri ? 'h-[600px]' : 'h-screen'} flex justify-center items-center bordernone border-tertiary rounded-big`}>
+        {isTauri && <div className='h-12 block'><Titlebar /></div>}
         <div className='w-full flex justify-center items-center h-full '>
         <MCDUContextProvider>
           {children}
-          </MCDUContextProvider>
+        </MCDUContextProvider>
           </div>
       </div>
       

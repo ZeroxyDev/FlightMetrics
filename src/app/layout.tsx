3a16692d0b83ed4814de1f686649b5a1ec68f3e5
ZeroxyDev <ZeroxyDev@gmail.com>
2024-02-10 19:53:40 +0100

    [Mod] - Footer only in App

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
           {children}
         </MCDUContextProvider>
           </div>
-          <Footer/>
+          {isTauri && <Footer/>}
       </div>
       </body>
     </html>
